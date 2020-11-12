36261325b636473262cec01681f6d0d62583cdac
Elin Vängbo <elinvangbo@yahoo.se>
2020-11-12 20:26:32 +0100

    update2

--- week3/LABMakingpredictionswithlogisticregression.sql
+++ week3/LABMakingpredictionswithlogisticregression.sql
@@ -5,7 +5,7 @@ USE sakila;
 
 #start with making a general table 
 SELECT f.film_id, f.title, f.description, fc.category_id,
-f.language_id, f.length/60 as hours, f.rental_duration,
+f.language_id, f.length/60 as hours_length, f.rental_duration,
 f.release_year, f.rating, 
 f.special_features, 
 ROUND(AVG(f.rental_duration)) * 24 AS avg_hours_rental_allowed,
@@ -16,6 +16,17 @@ JOIN film_category fc USING(film_id)
 JOIN film_actor fa USING(film_id)
 GROUP BY 1,2,3,4,5,6,7,8,9,10
 ORDER BY 1, 11;  
+
+#secondquery 
+SELECT 
+i.film_id,
+COUNT(r.rental_id) AS num_rented_times,
+p.amount AS rental_cost,
+AVG(TIMESTAMPDIFF(hour, r.rental_date, r.return_date)) AS avg_hours_rented
+FROM rental r
+JOIN inventory i USING(inventory_id)
+JOIN payment p USING(rental_id)
+GROUP BY i.film_id, rental_cost;
 
 
 SELECT r.rental_date, i.film_id, count(distinct r.inventory_id) FROM rental r
